632038abbb97363c2edc0f2ee95b7cbc7f149fff
Bex B <bexb1@hotmail.com>
2020-05-12 11:27:48 +0100

    Action types file added.

---
+++ src/actions/types.js
@@ -0,0 +1,10 @@
+// Authentication / users
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAILURE = "LOGIN_FAILURE";
+export const END_SESSION = "END_SESSION";
+export const START_SESSION = "START_SESSION";
+
+// Polls
+export const ADD_QUESTION = "ADD_QUESTION";
+
+export const ADD_ANSWER = "ADD_ANSWER";
